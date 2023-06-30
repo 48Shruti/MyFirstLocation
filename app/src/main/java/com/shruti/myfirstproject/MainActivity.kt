04552d49e43a24e70b492865a053498750e1c6d8
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2023-06-30 16:47:01 +0530

    Update main activity
    Validate name, phone number and about the college

--- app/src/main/java/com/shruti/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/shruti/myfirstproject/MainActivity.kt
@@ -2,10 +2,39 @@ package com.shruti.myfirstproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var etname : EditText?= null
+    var etcollegeinfo : EditText ?= null
+    var etphone : EditText ?= null
+    var btnvalidate : Button ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etname = findViewById(R.id.etname)
+        etcollegeinfo = findViewById(R.id.etcollegeinfo)
+        etphone = findViewById(R.id.etphone)
+        btnvalidate = findViewById(R.id.btnvalidate)
+      btnvalidate?.setOnClickListener {
+          if (etname?.text.isNullOrEmpty())
+          {
+              etname?.error= "Enter your name"
+          }else if (etcollegeinfo?.text.isNullOrEmpty())
+          {
+              etcollegeinfo?.error = "Enter about  your college"
+          }else if (etphone?.text.isNullOrEmpty())
+          {
+              etphone?.error = "Enter your phone number"
+          }else if(etphone?.text.toString().length<10){
+              etphone?.error = "Enter a validate number"
+          }else{
+              Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show()
+          }
+      }
+
     }
+
 }
